50c7577719c956c97b7ed2beaf3e5be4204ba36d
ajchen97 <19737483+ajchen97@users.noreply.github.com>
2023-03-06 19:44:41 -0800

    Update and rename queries to dannys_diner_queries.sql

--- 8-week-sql-challenge/01_dannys_diner/dannys_diner_queries.sql
+++ 8-week-sql-challenge/01_dannys_diner/dannys_diner_queries.sql
@@ -1,8 +1,31 @@
 -- https://8weeksqlchallenge.com/case-study-1/
 
 -- 1. What is the total amount each customer spent at the restaurant?
+SELECT sales.customer_id AS customer_id, 
+    SUM(menu.price) AS total_spent
+FROM dannys_diner.sales
+LEFT JOIN dannys_diner.menu
+ON sales.product_id = menu.product_id
+GROUP BY 1
+ORDER BY 1
+
 -- 2. How many days has each customer visited the restaurant?
+SELECT customer_id, 
+	COUNT(DISTINCT order_date) AS days_visited
+FROM dannys_diner.sales
+GROUP BY 1
+
 -- 3. What was the first item from the menu purchased by each customer?
+SELECT customer_id, product_name
+FROM (
+  SELECT *,
+      ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS row_num
+  FROM dannys_diner.sales
+  LEFT JOIN dannys_diner.menu
+  ON sales.product_id = menu.product_id
+  ORDER BY customer_id, order_date) items
+WHERE row_num = 1
+
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
