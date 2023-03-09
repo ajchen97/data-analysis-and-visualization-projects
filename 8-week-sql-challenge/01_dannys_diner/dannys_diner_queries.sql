f5ec17e7876d568cc814e7115a5454984e6f498a
Amanda <19737483+ajchen97@users.noreply.github.com>
2023-03-08 18:39:01 -0800

    Update dannys_diner_queries.sql

--- 8-week-sql-challenge/01_dannys_diner/dannys_diner_queries.sql
+++ 8-week-sql-challenge/01_dannys_diner/dannys_diner_queries.sql
@@ -10,13 +10,11 @@ ON sales.product_id = menu.product_id
 GROUP BY 1
 ORDER BY 1;
 
-
 -- 2. How many days has each customer visited the restaurant?
 SELECT customer_id, 
     COUNT(DISTINCT order_date) AS days_visited
 FROM dannys_diner.sales
 GROUP BY 1;
-
 
 -- 3. What was the first item from the menu purchased by each customer?
 SELECT DISTINCT customer_id, product_name
@@ -28,7 +26,6 @@ FROM (
   ON sales.product_id = menu.product_id
   ORDER BY customer_id, order_date) items
 WHERE ranking = 1;
-
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 WITH items AS (
@@ -44,7 +41,6 @@ FROM items
 WHERE purchase_count = (
   SELECT MAX(purchase_count)
   FROM items);
-  
   
 -- 5. Which item was the most popular for each customer?
 WITH items AS (
@@ -67,7 +63,6 @@ SELECT customer_id, product_name
 FROM ranking
 WHERE ranking = 1;
 
-
 -- 6. Which item was purchased first by the customer after they became a member?
 WITH customers AS (
   SELECT sales.customer_id AS customer_id, 
@@ -86,7 +81,6 @@ WITH customers AS (
 SELECT customer_id, product_name
 FROM customers
 WHERE ranking = 1;
-
 
 -- 7. Which item was purchased just before the customer became a member?
 WITH customers AS (
@@ -107,7 +101,6 @@ SELECT customer_id, product_name
 FROM customers
 WHERE ranking = 1;
 
-
 -- 8. What is the total items and amount spent for each member before they became a member?
 SELECT sales.customer_id AS customer_id, 
     COUNT(menu.product_name) AS total_items,
@@ -121,7 +114,6 @@ ON sales.product_id = menu.product_id
 GROUP BY 1
 ORDER BY 1;
 
-
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 SELECT sales.customer_id,
     SUM(CASE WHEN menu.product_name = 'sushi' THEN 20*menu.price ELSE 10*menu.price END) AS points
@@ -130,7 +122,6 @@ JOIN dannys_diner.menu
 ON sales.product_id = menu.product_id
 GROUP BY 1
 ORDER BY 1;
-
 
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 WITH points AS (
