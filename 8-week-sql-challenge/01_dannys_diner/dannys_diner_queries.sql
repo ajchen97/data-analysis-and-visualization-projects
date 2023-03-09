a9f7b0c3e05dbd7d29c755f49f755e301a0909ab
Amanda <19737483+ajchen97@users.noreply.github.com>
2023-03-08 16:07:55 -0800

    Update dannys_diner_queries.sql

--- 8-week-sql-challenge/01_dannys_diner/dannys_diner_queries.sql
+++ 8-week-sql-challenge/01_dannys_diner/dannys_diner_queries.sql
@@ -22,7 +22,7 @@ GROUP BY 1;
 SELECT DISTINCT customer_id, product_name
 FROM (
   SELECT *,
-  	RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS ranking
+    RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS ranking
   FROM dannys_diner.sales
   LEFT JOIN dannys_diner.menu
   ON sales.product_id = menu.product_id
@@ -33,7 +33,7 @@ WHERE ranking = 1;
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 WITH items AS (
   SELECT menu.product_name,
-  	COUNT(sales.product_id) AS purchase_count
+    COUNT(sales.product_id) AS purchase_count
   FROM dannys_diner.sales
   LEFT JOIN dannys_diner.menu
   ON sales.product_id = menu.product_id 
@@ -49,8 +49,8 @@ WHERE purchase_count = (
 -- 5. Which item was the most popular for each customer?
 WITH items AS (
   SELECT customer_id, 
-	product_name,
-  	COUNT(product_name) AS purchases
+    product_name,
+    COUNT(product_name) AS purchases
   FROM dannys_diner.sales
   LEFT JOIN dannys_diner.menu
   ON sales.product_id = menu.product_id 
@@ -58,9 +58,9 @@ WITH items AS (
   ORDER BY 1,3 DESC),
 ranking AS (
   SELECT customer_id, 
-	product_name, 
-	purchases,
-  	RANK() OVER (PARTITION BY customer_id ORDER BY purchases DESC) AS ranking
+    product_name, 
+    purchases,
+    RANK() OVER (PARTITION BY customer_id ORDER BY purchases DESC) AS ranking
   FROM items)
 
 SELECT customer_id, product_name
@@ -71,10 +71,10 @@ WHERE ranking = 1;
 -- 6. Which item was purchased first by the customer after they became a member?
 WITH customers AS (
   SELECT sales.customer_id AS customer_id, 
-  	sales.order_date, 
-	members.join_date,
-  	menu.product_name AS product_name,
-  	RANK() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date) AS ranking
+    sales.order_date, 
+    members.join_date,
+    menu.product_name AS product_name,
+    RANK() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date) AS ranking
   FROM dannys_diner.sales
   JOIN dannys_diner.members
   ON sales.customer_id = members.customer_id
@@ -91,10 +91,10 @@ WHERE ranking = 1;
 -- 7. Which item was purchased just before the customer became a member?
 WITH customers AS (
   SELECT sales.customer_id AS customer_id, 
-      sales.order_date, 
-      members.join_date,
-      menu.product_name AS product_name,
-      RANK() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date DESC) AS ranking
+    sales.order_date, 
+    members.join_date,
+    menu.product_name AS product_name,
+    RANK() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date DESC) AS ranking
   FROM dannys_diner.sales
   JOIN dannys_diner.members
   ON sales.customer_id = members.customer_id
@@ -135,13 +135,13 @@ ORDER BY 1;
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 WITH points AS (
   SELECT sales.customer_id, 
-  	sales.order_date, 
-  	members.join_date, 
-  	menu.product_name, 
-  	menu.price,
-  	CASE WHEN (sales.order_date < members.join_date OR sales.order_date > members.join_date + 7) AND menu.product_name = 'sushi' THEN 20*menu.price
-  	  WHEN (sales.order_date < members.join_date OR sales.order_date > members.join_date + 7) AND menu.product_name != 'sushi' THEN 10*menu.price 
-  	  WHEN sales.order_date BETWEEN members.join_date AND members.join_date + 7 THEN 20*menu.price END AS points
+    sales.order_date, 
+    members.join_date, 
+    menu.product_name, 
+    menu.price,
+    CASE WHEN (sales.order_date < members.join_date OR sales.order_date > members.join_date + 7) AND menu.product_name = 'sushi' THEN 20*menu.price
+      WHEN (sales.order_date < members.join_date OR sales.order_date > members.join_date + 7) AND menu.product_name != 'sushi' THEN 10*menu.price 
+      WHEN sales.order_date BETWEEN members.join_date AND members.join_date + 7 THEN 20*menu.price END AS points
   FROM dannys_diner.sales
   JOIN dannys_diner.members
   ON sales.customer_id = members.customer_id
