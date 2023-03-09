e29aa9d65390504e5345123661b289a0fe3cf156
Amanda <19737483+ajchen97@users.noreply.github.com>
2023-03-08 18:38:03 -0800

    Update pizza_runner_queries.sql

--- 8-week-sql-challenge/02_pizza_runner/pizza_runner_queries.sql
+++ 8-week-sql-challenge/02_pizza_runner/pizza_runner_queries.sql
@@ -1,7 +1,7 @@
 -- https://8weeksqlchallenge.com/case-study-2/
 -- PostgreSQL
 
--- A. Pizza Metrics
+-- A. PIZZA METRICS
 -- 1. How many pizzas were ordered?
 SELECT COUNT(*) AS total_pizzas
 FROM customer_orders_temp;
@@ -109,14 +109,22 @@ ORDER BY 2;
 
 
 
--- B. Runner and Customer Experience
+-- B. RUNNER AND CUSTOMER EXPERIENCE
 -- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+
 -- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
 -- 3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
+
 -- 4. What was the average distance travelled for each customer?
+
 -- 5. What was the difference between the longest and shortest delivery times for all orders?
+
 -- 6. What was the average speed for each runner for each delivery and do you notice any trend for these values?
+
 -- 7. What is the successful delivery percentage for each runner?
+
+
 
 -- C. Ingredient Optimisation
 -- 1. What are the standard ingredients for each pizza?
