3b2440ed3d8f91b47bffa5801de40a3040425817
Amanda <19737483+ajchen97@users.noreply.github.com>
2023-03-27 20:30:48 -0700

    Update 03_foodie-fi_queries.sql

--- 8-week-sql-challenge/03_foodie-fi/03_foodie-fi_queries.sql
+++ 8-week-sql-challenge/03_foodie-fi/03_foodie-fi_queries.sql
@@ -1,5 +1,4 @@
 -- https://8weeksqlchallenge.com/case-study-3/
--- PostgreSQL
 
 -- A. Customer Journey
 -- 1. Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey. (Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!) </details>
