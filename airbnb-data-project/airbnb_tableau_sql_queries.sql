5ec869584f1015629f04f502cd04c019c003a1e3
Amanda <19737483+ajchen97@users.noreply.github.com>
2023-08-17 17:44:37 -0700

    Update airbnb_tableau_sql_queries.sql

--- airbnb-data-project/airbnb_tableau_sql_queries.sql
+++ airbnb-data-project/airbnb_tableau_sql_queries.sql
@@ -17,42 +17,42 @@ ORDER BY 1;
 
 -- Hosts per Neighbourhood
 SELECT neighbourhood,
-	COUNT(DISTINCT host_id) AS host_count
+  COUNT(DISTINCT host_id) AS host_count
 FROM listings
 GROUP BY 1
 ORDER BY 1;
 
 -- Listings per Neighbourhood
 SELECT neighbourhood, 
-	COUNT(id) AS listings_count
+  COUNT(id) AS listings_count
 FROM listings
 GROUP BY 1
 ORDER BY 1;
 
 -- Avg Price per Neighbourhood
 SELECT neighbourhood, 
-	ROUND(AVG(price), 2) AS avg_price_per_neighbourhood
+  ROUND(AVG(price), 2) AS avg_price_per_neighbourhood
 FROM listings
 GROUP BY 1
 ORDER BY 1;
 
 -- Room Type Listings Count
 SELECT room_type,
-	COUNT(id) AS listings_count,
-	COUNT(id)::numeric/(SELECT COUNT(*) FROM listings) AS listings_percentage
+  COUNT(id) AS listings_count,
+  COUNT(id)::numeric/(SELECT COUNT(*) FROM listings) AS listings_percentage
 FROM listings
 GROUP BY 1;
 
 -- Avg Price per Room Type Listing
 SELECT room_type,
-	ROUND(AVG(price), 2) AS avg_price_per_room_type
+  ROUND(AVG(price), 2) AS avg_price_per_room_type
 FROM listings
 GROUP BY 1;
 
 -- Bedroom Listings Count
 SELECT bedrooms,
-	COUNT(id) AS listings_count,
-	COUNT(id)::numeric/(SELECT COUNT(*) FROM listings) AS listings_percentage
+  COUNT(id) AS listings_count,
+  COUNT(id)::numeric/(SELECT COUNT(*) FROM listings) AS listings_percentage
 FROM listings
 WHERE bedrooms IS NOT NULL
 GROUP BY 1
