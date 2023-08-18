96ac9504bf1ccbbd894bba473ff76516c81ce994
Amanda <19737483+ajchen97@users.noreply.github.com>
2023-08-18 14:04:50 -0700

    Update airbnb_tableau_sql_queries.sql

--- airbnb-data-project/airbnb_tableau_sql_queries.sql
+++ airbnb-data-project/airbnb_tableau_sql_queries.sql
@@ -15,27 +15,6 @@ FROM updated_calendar
 GROUP BY 1
 ORDER BY 1;
 
--- Hosts per Neighbourhood
-SELECT neighbourhood,
-  COUNT(DISTINCT host_id) AS host_count
-FROM listings
-GROUP BY 1
-ORDER BY 1;
-
--- Listings per Neighbourhood
-SELECT neighbourhood, 
-  COUNT(id) AS listings_count
-FROM listings
-GROUP BY 1
-ORDER BY 1;
-
--- Avg Price per Neighbourhood
-SELECT neighbourhood, 
-  ROUND(AVG(price), 2) AS avg_price_per_neighbourhood
-FROM listings
-GROUP BY 1
-ORDER BY 1;
-
 -- Room Type Listings Count
 SELECT room_type,
   COUNT(id) AS listings_count,
@@ -67,3 +46,16 @@ WHERE bedrooms IS NOT NULL
 GROUP BY 1
 HAVING COUNT(bedrooms) > 10
 ORDER BY 1;
+
+-- Instant Bookable Listings
+WITH instant_book AS (
+  SELECT room_type,
+    instant_bookable,
+    DENSE_RANK() OVER (PARTITION BY room_type ORDER BY instant_bookable DESC) AS book_rank
+  FROM listings)
+
+SELECT room_type,
+  COUNT(book_rank) FILTER (WHERE book_rank = 1) AS yes_instant_bookable,
+  COUNT(book_rank) FILTER (WHERE book_rank = 2) AS not_instant_bookable
+FROM instant_book
+GROUP BY 1;
